4f334c62cc57d067c581c2e82bc19ba25fb071f8
genti91 <franco.g7@hotmail.com>
2023-03-08 20:10:58 -0300

    .config

--- config/sublime-text/Packages/Theme - Rainbow/rainbow_lib/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core utilities for Rainbow Theme
-"""
-
-from . import utils
-from .logging import logger, setup_logger
-from .fsi import FileSystemInterfacer
--- config/sublime-text/Packages/Theme - Rainbow/reloader_command.py
+++
@@ -1,8 +0,0 @@
-import sublime_plugin
-
-
-class RainbowReloadPackageCommand(sublime_plugin.ApplicationCommand):
-
-    def run(self):
-        sublime_plugin.reload_plugin("Theme - Rainbow.1_reloader")
-        sublime_plugin.reload_plugin("Theme - Rainbow.main")
